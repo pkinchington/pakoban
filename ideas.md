08618e122c19b5d37f2bdc551548de080cb82e84
Philip Kinchington <philip@kinchington.com>
2012-06-24 15:56:48 +0100

    Expanded functions and styling

--- ideas.md
+++ ideas.md
@@ -1,7 +1,9 @@
-Ideas
-=====
+# Ideas
 
-Boards are just a grid - steal the checkers board code from the Codecademy lessons and mung it into something that can be used to draw a level.
+**Note** I have included the development version of jQuery-1.7.2. This should really be swapped for a CDN hosted version in the final?
+
+## Boards are just a grid
+Steal the checkers board code from the Codecademy lessons and mung it into something that can be used to draw a level.
 How to define/load level layout? String where W=Wall, F=Floor, P=Player Start Position, B=Box, D=Destination, Z=Box at destination, -=Blank?
 
 --WWWWW-
@@ -15,3 +17,8 @@ WFFFDFFW
 WWWWWWWW
 
 This is the level from the Wikipedia article on Sokoban.
+
+Maybe encode it into a single line with the size of the grid first:
+
+8x9:--WWWWW-WWWFFFW-WDPBFFW.... etc.
+
--- pakoban.js
+++ pakoban.js
@@ -1,3 +1,60 @@
+
+
+function setupBoard() {
+
+  function parseLevel() {
+    // Start with a fixed level. Replace this later with a levelCode passed
+    // in after being read from somewhere.
+    var levelCode = 8x9:--WWWWW-WWWFFFW-WDPBFFW-WWWFBDW-WDWWBFW-WFWFDFWWWBFZBBDWWFFFDFFWWWWWWWWW;
+
+    var data = levelCode.split(':'),
+        levelDims = levelData[0].split('x'),
+        boardWidth = parseInt(levelDims[0], 10),
+        boardHeight = parseInt(levelDims[1], 10),
+        levelData = data[1];
+  }
+
+  function getTileType (index) {
+    var tile;
+
+    switch (tile) {
+      case '-':
+        return 'blank';
+        break;
+      case 'W':
+        return 'wall';
+        break;
+      case 'F':
+        return 'floor';
+        break;
+      case 'P':
+        return 'player';
+        break;
+      case 'B':
+        return 'box';
+        break;
+      case 'D':
+        return 'destination';
+        break;
+      case 'Z':
+        return 'boxcomplete';
+        break;
+      default:
+        return 'unknown';
+    }
+  }
+
+  $('div.tile').each(function(index, tile) {
+    $(tile).addClass(getTileType(index));
+  }
+}
+
+function incrementMoveCount() {
+  var curMoveCount = parseInt($('#moveCount').html(),10);
+  curMoveCount++;
+  $('#moveCount').html(curMoveCount);
+}
+
 // global variables for one tile
 var width = 46;
 var border = 2;
